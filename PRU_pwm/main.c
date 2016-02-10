dca4ef392c1a43c2386a63860b2404b4fe85291f
Jelle Spijker <spijker.jelle@gmail.com>
2016-02-10 17:30:07 +0100

    Added PRU_PWM test program

---
+++ PRU_pwm/main.c
@@ -0,0 +1,23 @@
+/***************************************************************************************
+ * MAIN.C
+ *
+ * Description: main source file for PRU development
+ *
+ * Rafael de Souza
+ * (C) 2015 Texas Instruments, Inc
+ * 
+ * Built with Code Composer Studio v6
+ **************************************************************************************/
+
+#include <stdint.h>
+#include "resource_table_empty.h"
+
+int main(void)
+{
+	/* Your code goes here */
+
+	__halt();
+	
+	/* Should never return */
+	return 0;
+}
