1544865f2cc9f1b377abdefebe7737a6d19bea41
Jelle Spijker <spijker.jelle@gmail.com>
2016-02-28 20:24:53 +0100

    PRU code added

--- PRU_pwm/main.c
+++ PRU_pwm/main.c
@@ -10,12 +10,28 @@
  **************************************************************************************/
 
 #include <stdint.h>
+#include <pru_cfg.h>
+#include <pru_ctrl.h>
 #include "resource_table_empty.h"
 
-int main(void)
-{
-	/* Your code goes here */
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
 
+void setPin(register uint32_t) {
+
+}
+
+int main(void) {
+	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	/* TODO: Create stop condition, else it will toggle indefinitely */
+	while(1){
+		__R30 ^= 1 << 1;
+		__delay_cycles(100000000);
+	}
+
+	/* Halt the PRU core */
 	__halt();
 	
 	/* Should never return */
